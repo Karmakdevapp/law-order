e74ad7ee1dbcc022e434b6f1ea0b12c1bc9f1049
gastonnrodriguez <gastonnrodriguez@gmail.com>
2022-04-25 23:37:26 -0300

    se agrega funcion de esconder whatsapp fuera del horario de oficina

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
-
+const wa = document.getElementById("whatsapp");
+let time = new Date();
+let hours = time.getUTCHours();
+let day = time.getUTCDay();
+if (day != 6 && day != 0) {
+  if (hours >= 9 && hours <= 18) {
+    wa.style.display = "block";
+  }
+}
